ac8197a27489fa2cec3616acb4f3f4a871c03e3d
Ricardoq <azevedoricardo98@ua.pt>
2021-05-30 18:27:46 +0100

    setup spring rest api

---
+++ backend/src/main/java/es_ontrack/backend/src/SrcApplication.java
@@ -0,0 +1,13 @@
+package es_ontrack.backend.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcApplication.class, args);
+	}
+
+}
