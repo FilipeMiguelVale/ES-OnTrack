16d251a926808ca53c27e3fb4519bd3915f80448
Rafael Santos <r.c.santos@ua.pt>
2021-06-07 14:59:39 +0100

    Added Initial Kafka Producer Initialization. Missing Bus Type Kafka Serializer

--- bus-producer/src/main/java/com/EsOntrack/busproducer/BusProducer.java
+++ bus-producer/src/main/java/com/EsOntrack/busproducer/BusProducer.java
@@ -5,16 +5,33 @@
  */
 package com.EsOntrack.busproducer;
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import com.EsOntrack.busproducer.Bus;
+import java.util.Properties;
 /**
  *
  * @author adven
  */
 public class BusProducer extends Thread {
     
-    int port;
-    String address;
-    String topic;
+    public final String BOOTSTRAP_SERVERS;
+    public final String topic;
+    private Producer<String,Bus> prod;
     
+    public BusProducer(String serv,String top)
+    {
+    this.BOOTSTRAP_SERVERS=serv;
+    this.topic=top;
+    }
     
+    private Producer<String,Bus> createProducer()
+    {
+    Properties prop = new Properties();
+    prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,BOOTSTRAP_SERVERS);
+    prop.put(ProducerConfig.CLIENT_ID_CONFIG, "Kafka Example Producer");
+    prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+    prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,)
+    return new KafkaProducer<>(prop);
+    }
     
 }
