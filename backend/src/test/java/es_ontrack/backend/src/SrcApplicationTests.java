c536d80b89c011878a3fef6e084cae9f2a3979c8
Daniel Magueta <daniel.magueta@hotmail.com>
2021-06-07 10:37:58 +0100

    jenkinsfile exists test, dummy test

--- backend/src/test/java/es_ontrack/backend/src/SrcApplicationTests.java
+++ backend/src/test/java/es_ontrack/backend/src/SrcApplicationTests.java
@@ -3,11 +3,21 @@ package es_ontrack.backend.src;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
 @SpringBootTest
 class SrcApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+    @Test
+	void DockerFileExists() {
+
+		File tmp = new File("Dockerfile");
+		assert (tmp.exists());
+		System.out.println("DockerFile exists!");
 	}
 
 }
