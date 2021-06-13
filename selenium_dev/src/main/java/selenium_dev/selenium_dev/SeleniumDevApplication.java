84cc450b97fa152816761c66293b5919443fc6b3
Daniel Magueta <daniel.magueta@hotmail.com>
2021-06-13 22:02:56 +0100

    selenium directory added

---
+++ selenium_dev/src/main/java/selenium_dev/selenium_dev/SeleniumDevApplication.java
@@ -0,0 +1,13 @@
+package selenium_dev.selenium_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumDevApplication.class, args);
+	}
+
+}
---
+++ selenium_dev/src/test/java/selenium_dev/selenium_dev/SeleniumDevApplicationTests.java
@@ -0,0 +1,13 @@
+package selenium_dev.selenium_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
