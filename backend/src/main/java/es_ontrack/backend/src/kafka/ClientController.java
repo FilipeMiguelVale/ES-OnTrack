47e20d03fb2e80189f7cca738ad55c78887b1dae
Filipe Vale <fmmvale@hotmail.com>
2021-06-19 16:15:47 +0100

    test elk

--- backend/src/main/java/es_ontrack/backend/src/kafka/ClientController.java
+++ backend/src/main/java/es_ontrack/backend/src/kafka/ClientController.java
@@ -7,18 +7,27 @@ package es_ontrack.backend.src.kafka;
 
 import java.util.HashMap;
 import java.util.List;
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es_ontrack.backend.src.api.RestControler;
 import es_ontrack.backend.src.models.Bus;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 @RestController
+@Log4j2
 @RequestMapping("/test")
 public class ClientController {
     @Autowired
     private KafkaCons consumer;
-
+    
+    private static final Logger LOG = Logger.getLogger(ClientController.class.getName());
+    
     @RequestMapping("/test")
     public String test(){
         return "test";
@@ -31,6 +40,9 @@ public class ClientController {
 
     @RequestMapping("/data")
     public HashMap<String, Bus> getData(){
+    	//log.info("Topic: {}", "plane");
+    	LOG.log(Level.INFO, "teste");
+    	LOG.log(Level.INFO, consumer.getBuses().toString());
         return consumer.getBuses();
     }
 }
