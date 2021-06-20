fdd2e0c3bcce980cf64af2972b28c783ddf0293a
Filipe Vale <fmmvale@hotmail.com>
2021-06-20 10:48:25 +0100

    fixed import

--- backend/src/main/java/es_ontrack/backend/src/kafka/KafkaCons.java
+++ backend/src/main/java/es_ontrack/backend/src/kafka/KafkaCons.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
